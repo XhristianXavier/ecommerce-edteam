479345bcf1f193aaa7a6617b05853d72de666afe
Christian Javier <chrisjavierps@hotmail.com>
2023-02-22 15:08:02 -0600

    Se agrega router, links al menu y la configuracion inicial de Tailwindcss

---
+++ src/components/pages/Products.jsx
@@ -0,0 +1,14 @@
+import MainHeader from "../organisms/MainHeader"
+
+const Products = () => {
+    return (
+        <>
+            <div>
+                <h1>Products</h1>
+                <p>Explora todos nuestros productos</p>
+            </div>
+        </>
+    )
+}
+
+export default Products
